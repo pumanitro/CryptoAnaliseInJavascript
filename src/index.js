eca368faa521554063f16d8ce7f3112eb8ee8879
Patryk Janik <patryk.janik@castingnetworks.com>
2019-03-02 23:36:45 +0100

    Connected redux and sagas

--- src/index.js
+++ src/index.js
@@ -1,10 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import { Provider } from "react-redux";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { createStore, compose, applyMiddleware } from "redux";
+import combinedReducers from "reducers";
+import createSagaMiddleware from "redux-saga";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const sagaMiddleware = createSagaMiddleware();
+
+const composeEnhancers =
+  process.env.NODE_ENV !== "production" &&
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+        name: "Cryto Analise In JavaScript"
+      })
+    : compose;
+
+const store = createStore(
+  combinedReducers,
+  composeEnhancers(applyMiddleware(sagaMiddleware))
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
