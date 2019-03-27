c22efe104b4d58a617b617430e9eab57d7369258
Patryk Janik <patryk.janik@castingnetworks.com>
2019-03-27 20:51:08 +0100

    Added Mouse value indicator to Chart

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -3,12 +3,14 @@ import PropTypes from "prop-types";
 
 import { scaleTime } from "d3-scale";
 import { utcMinute } from "d3-time";
+import { format } from "d3-format";
 
 import { ChartCanvas, Chart } from "react-stockcharts";
 import { CandlestickSeries } from "react-stockcharts/lib/series";
 import { XAxis, YAxis } from "react-stockcharts/lib/axes";
 import { fitWidth } from "react-stockcharts/lib/helper";
 import { last, timeIntervalBarWidth } from "react-stockcharts/lib/utils";
+import { MouseCoordinateY } from "react-stockcharts/lib/coordinates";
 
 class CandleStickChart extends React.Component {
   render() {
@@ -34,6 +36,13 @@ class CandleStickChart extends React.Component {
         <Chart id={1} yExtents={d => [d.high, d.low]}>
           <XAxis axisAt="bottom" orient="bottom" ticks={6} />
           <YAxis axisAt="left" orient="left" ticks={5} />
+
+          <MouseCoordinateY
+            at="right"
+            orient="right"
+            displayFormat={format(".2f")}
+          />
+
           <CandlestickSeries width={timeIntervalBarWidth(utcMinute)} />
         </Chart>
       </ChartCanvas>
