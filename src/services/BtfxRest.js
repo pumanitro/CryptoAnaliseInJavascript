28372b582ae06445a92d78034f583ba702b0bbe7
Patryk Janik <patryk.janik@castingnetworks.com>
2019-03-27 19:51:07 +0100

    Coppied BTFREST and Utils from prevous project

---
+++ src/services/BtfxRest.js
@@ -0,0 +1,38 @@
+import axios from "axios";
+import { dateToTimeStamp, timeFrameToMS } from "utils/BtfxUtils";
+
+class BtfxRest {
+  REST_URL = "https://api.bitfinex.com/v2/";
+  MAX_CANDLES_AMOUNT = 1000;
+
+  getCandles = (timeFrame, symbol, dateStart, dateEnd) => {
+    // 1388534400000
+
+    return axios
+      .get(
+        `${
+          this.REST_URL
+        }candles/trade:${timeFrame}:${symbol}/hist?start=${dateToTimeStamp(
+          dateStart
+        )}&end=${dateToTimeStamp(dateEnd)}&limit=${this.MAX_CANDLES_AMOUNT}`
+      )
+      .then(resp => {
+        return resp.data;
+      })
+      .catch(err => {
+        console.warn(err);
+      });
+  };
+
+  getMoreCandles = (timeFrame, symbol, lastCandle) => {
+    const end = lastCandle.date;
+
+    const start = new Date(
+      end - timeFrameToMS(timeFrame) * this.MAX_CANDLES_AMOUNT
+    );
+
+    return this.getCandles(timeFrame, symbol, start, end);
+  };
+}
+
+export default new BtfxRest();
--- src/utils/BtfxUtils.js
+++ src/utils/BtfxUtils.js
@@ -16,3 +16,28 @@ export function parseCandles(candles) {
 export function dateToTimeStamp(date) {
   return Math.floor(date / 1);
 }
+
+export function timeFrameToMS(timeFrame) {
+  let secondMultiplier;
+
+  switch (timeFrame[timeFrame.length - 1]) {
+    case "m":
+      secondMultiplier = 60;
+      break;
+    case "h":
+      secondMultiplier = 60 * 60;
+      break;
+    case "D":
+      secondMultiplier = 60 * 60 * 12;
+      break;
+    case "M":
+      secondMultiplier = 60 * 60 * 12 * 29;
+      break;
+    default:
+      throw "You need to set time frame with m/h/D/M sings at the end";
+  }
+
+  const times = +timeFrame.substring(0, timeFrame.length - 1);
+
+  return secondMultiplier * 1000 * times;
+}
